77bc13383104b5686aa78e36b1bf915d37c34df0
MODcraft <admin@ncgs.top>
2025-06-08 12:31:30 +0800

    Remove prefix

--- lua/azlink/commands/azlink_command.lua
+++ lua/azlink/commands/azlink_command.lua
@@ -10,7 +10,7 @@ end
 
 concommand.Add( "azlink", function( ply, _, args )
     if IsValid( ply ) then
-        ply:SendLua( [[MsgC(Color(255,0,0),"[AzLink - ERROR] ",color_white,"This command must be executed from the server console.","\n")]] )
+        ply:PrintMessage( HUD_PRINTCONSOLE, "This command must be executed from the server console." )
         return
     end
 
