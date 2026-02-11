96d552666a154e890c220cb97b06fe50539d1b40
MrMicky <git@mrmicky.fr>
2026-02-11 08:02:38 +0100

    Fallback to default HTTP if CHTTP is not installed

--- lua/azlink/http/http_client.lua
+++ lua/azlink/http/http_client.lua
@@ -1,10 +1,17 @@
 local httpClient = {
     timeout = 5000
 }
+local httpRequest
 
-function httpClient:Request(requestMethod, endpoint, data)
-    return AzLink.Promise(function(onResolve, onReject)
-        local request = CHTTP({
+if pcall( require, "chttp" ) and CHTTP ~= nil then
+	httpRequest = CHTTP
+else
+	httpRequest = HTTP
+end
+
+function httpClient:Request( requestMethod, endpoint, data )
+    return AzLink.Promise( function( onResolve, onReject )
+        httpRequest( {
             method = requestMethod,
             url = AzLink.config.url .. "/api/azlink" .. endpoint,
             timeout = self.timeout,
@@ -13,22 +20,22 @@ function httpClient:Request(requestMethod, endpoint, data)
                 ["Accept"] = "application/json",
                 ["Content-Type"] = "application/json",
             },
-            body = data and util.TableToJSON(data) or nil,
-            success = function(code, body, headers)
-                local jsonBody = body and util.JSONToTable(body) or body
+            type = "application/json",
+            body = data and util.TableToJSON( data ) or nil,
+            success = function( code, body )
+                local jsonBody = body and util.JSONToTable( body ) or body
 
                 if code >= 300 then
-                    onReject(jsonBody.message or body, code)
+                    onReject( jsonBody.message or body, code )
+
                     return
                 end
 
-                onResolve(jsonBody)
+                onResolve( jsonBody )
             end,
-            failed = function(error)
-                onReject(error)
-            end,
-        })
-    end)
+            failed = onReject,
+        } )
+    end )
 end
 
 AzLink.HttpClient = httpClient
